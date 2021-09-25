05066b0f838ef7e05dd2494c24693e7aaa056f9c
Chanha Kim <bluechanha@snu.ac.kr>
2021-09-25 19:09:08 +0900

    test

--- test/test.cpp
+++ test/test.cpp
@@ -3,43 +3,81 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+
+void init (void)
+{
+    glClearColor (0.0,0.0,0.0,0.0); //background color
+    glShadeModel(GL_FLAT);
+}
+
+static int year = 0, day = 0;
+
+
 void display(void)
 {
     // clear all pixels 
     glClear (GL_COLOR_BUFFER_BIT);
     
     //draw white polygon
-    glColor3f (1.0,1.0,1.0);
-    glBegin(GL_POLYGON);
-        glVertex3f (0.25,0.25,0.0);
-        glVertex3f (0.75,0.25,0.0);
-        glVertex3f (0.75,0.75,0.0);
-        glVertex3f (0.25,0.75,0.0);
-    glEnd();
+    
+    //glLoadIdentity();
+    glPushMatrix();
+        glColor3f (0.7,0.0,0.0);
+        glutWireSphere(1.0, 100, 100);
+        
+        glColor3f (0.0,0.0,0.7);
+        glRotatef((GLfloat) year,0.0,1.0,0.0);
+        glTranslatef(2.0,0.0,0.0);
+        //glScalef(1.0,2.0,1.0);
+        glutWireSphere(0.5, 50, 50);
 
-    glFlush(); //start processing buffered OpenGL routines
 
+    glPopMatrix();
+    glutSwapBuffers(); //start processing buffered OpenGL routines
+
+} 
+
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key) 
+{
+    case 'd':
+        year = (year + 10) % 360;
+        glutPostRedisplay();
+        break;
+    case 'f':
+        year = (year - 10) % 360;
+        glutPostRedisplay();
+        break;        
+}
 }
 
-void init (void)
-{
-    glClearColor (0.0,0.0,0.0,0.0); //background color
 
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
+    //glFrustum(-1.0,1.0,-1.0,1.0,1.5,20.0);
+    gluPerspective(45.0,1.0,1.5,20.0);
+    glMatrixMode(GL_MODELVIEW);
+    //glLoadIdentity();
+    gluLookAt(5.0,5.0,5.0,0.0,0.0,0.0,0.0,1.0,0.0);
+
 
 }
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE |GLUT_RGB);
-    glutInitWindowSize (250,250);
-    glutInitWindowPosition (100,100);
-    glutCreateWindow ("hello");
+    glutInitDisplayMode (GLUT_DOUBLE |GLUT_RGB);
+    glutInitWindowPosition (600,400);
+    glutInitWindowSize (800,800);
+    glutCreateWindow ("Just Practice");   
     init();
     glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
     glutMainLoop();
     
     return 0;
